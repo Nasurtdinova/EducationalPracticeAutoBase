a81be815bec9436a886513c48d7d70f75a380d33
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-10-21 19:01:57 +0300

    Update

--- MotorDepot/Pages/SearchDriverPage.xaml.cs
+++ MotorDepot/Pages/SearchDriverPage.xaml.cs
@@ -18,21 +18,43 @@ namespace MotorDepot
 {
     public partial class SearchDriverPage : Page
     {
+        public List<RequestDriver> UpdatedRequestDriver { get; set; }
         public SearchDriverPage()
         {
             InitializeComponent();
-            cbArrival.ItemsSource = DataAccess.GetCities();
-            cbDeparture.ItemsSource = DataAccess.GetCities();
+            var arrival = DataAccess.GetCities();
+            arrival.Insert(0, new City { Name = "Все" });
+            cbArrival.ItemsSource = arrival;
+            cbArrival.SelectedIndex = 0;
+
+            var depart = DataAccess.GetCities();
+            depart.Insert(0, new City { Name = "Все" });
+            cbDeparture.ItemsSource = depart;
+            cbDeparture.SelectedIndex = 0;
+
             tbData.DisplayDateStart = DateTime.Now;
             lvDrivers.ItemsSource = DataAccess.GetRequestDrivers().Where(a=>a.Data >= DateTime.Now);
         }
 
+        public void UpdateList()
+        {
+            UpdatedRequestDriver = DataAccess.GetRequestDrivers().Where(a => a.Data >= DateTime.Now).ToList();
+            if (cbArrival.SelectedIndex > 0)
+                UpdatedRequestDriver = UpdatedRequestDriver.Where(a => a.PlaceArrival.City == cbArrival.SelectedItem as City).ToList();
+            if (cbDeparture.SelectedIndex > 0)
+                UpdatedRequestDriver = UpdatedRequestDriver.Where(a => a.PlaceDeparture.City == cbDeparture.SelectedItem as City).ToList();
+            if (tbData.SelectedDate != null)
+                UpdatedRequestDriver = UpdatedRequestDriver.Where(a => a.Data == tbData.SelectedDate).ToList();
+            if (tbCount.Text != String.Empty)
+                UpdatedRequestDriver = UpdatedRequestDriver.Where(a => a.FreeVenue >= Convert.ToInt32(tbCount.Text)).ToList();
 
+            lvDrivers.ItemsSource = UpdatedRequestDriver;
+        }
 
         public void OnComboDepartureTextChanged(object sender, RoutedEventArgs e)
         {
             var city = DataAccess.GetCities();
-            cbDeparture.ItemsSource = city.Where(a => a.Name.ToLower().Contains(cbDeparture.Text.ToLower()));
+            cbDeparture.ItemsSource = city.Where(a => a.Name.ToLower().Contains(cbDeparture.Text.ToLower()));          
         }
 
         public void OnComboArrivalTextChanged(object sender, RoutedEventArgs e)
@@ -55,6 +77,26 @@ namespace MotorDepot
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void tbData_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void tbCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void cbArrival_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void cbDeparture_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
         }
     }
 }
